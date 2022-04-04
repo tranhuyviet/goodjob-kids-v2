79a9bd46fc4d0358bd749326e56c1d85ef68f069
tranhuyviet <vietth@msn.com>
2022-04-04 09:38:27 +0300

    Add get jobs done api

---
+++ pages/api/users/jobsdone/index.ts
@@ -0,0 +1,43 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import nc from 'next-connect';
+import checkAuth from '../../../../utils/checkAuth';
+import db from '../../../../utils/db';
+import errorParse from '../../../../utils/errorParse';
+import { resError, resSuccess } from '../../../../utils/returnRes';
+
+const handler = nc();
+
+handler.get(async (req: NextApiRequest, res: NextApiResponse) => {
+	try {
+		// connect db
+		await db.connect();
+
+		// check authentication
+		const user = await checkAuth(req);
+
+		if (!user) {
+			return resError(
+				res,
+				'Unauthorized',
+				{
+					global: 'You are not logged in',
+				},
+				401,
+			);
+		}
+
+		// disconnect db
+		await db.disconnect();
+
+		// return job done id have just added to jobsdone array
+		return resSuccess(res, { jobsDone: user.jobsDone });
+	} catch (error) {
+		if (error instanceof Error && error.name === 'ValidationError') {
+			const errors = errorParse(error);
+			return resError(res, 'Bad Request Error - Validate Input', errors, 400);
+		}
+		return resError(res, 'Something went wrong', { global: 'Something went wrong' }, 500);
+	}
+});
+
+export default handler;
